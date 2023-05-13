47458efe4e8b48a13a0d2751735f61cfd497853f
Gdecu <133132087+Gdecu@users.noreply.github.com>
2023-05-13 16:00:16 +0200

    Update blocs_circuit.py

--- Interprétation mesures/blocs_circuit.py
+++ Interprétation mesures/blocs_circuit.py
@@ -30,19 +30,18 @@ def plot_bloc1():
     
     
     # On plot les données dans un graphe
-    Value_1[0] -= 5
-    Value_2[0] += 1;  Value_2[0] *= 0.84
-    Title = ['Bloc 1 : Oscillateur', 'V_cc',  'V_s', 'V_G', 'V_D']
+    Value_1[0] -= 9.5
+    Value_2[0] += 0.675;  Value_2[0] *= 0.835
+    Title = ['Bloc 1 : Oscillateur', 'V_cc',  'V_S', 'V_G', 'V_D']
     fig, ax = plt.subplots()
-    fig.suptitle(Title[0])
     color = ['r', 'b', 'g', 'y', 'm', 'c', 'k']
-    ax.plot(Value_1[0], Value_1[1], color[0], label = Title[1])
-    ax.plot(Value_1[0], Value_1[2], color[1], label = Title[2])
+    ax.plot(Value_1[0], Value_1[2], color[1], label = Title[1])
+    ax.plot(Value_1[0], Value_1[1], color[0], label = Title[2])
     ax.plot(Value_2[0], Value_2[1], color[2], label = Title[3])
     ax.plot(Value_2[0], Value_2[2], color[3], label = Title[4])
     ax.legend()
     ax.set_ylim(-1, 6)
-    ax.set_xlim(0, 20)
+    ax.set_xlim(0, 17.5)
     ax.set_xlabel('Temps [µs]')
     ax.set_ylabel('Tensions [V]')
     plt.show()
@@ -58,7 +57,8 @@ def plot_bloc2():
     Value_moy = cs.data_moy([Value_1, Value_11, Value_12])
     
     # On plot les données dans un graphes
-    Title = ['Bloc 2 : Circuit RL', 'V_s', 'V_L']
+    Title = ['Bloc 2 : Circuit RL', 'V_S', 'V_L']
+    Value_moy[0] -= 0.32
     cs.graph(Title, Value_moy)
 
 def plot_bloc3():
@@ -72,34 +72,35 @@ def plot_bloc3():
     Value_moy = cs.data_moy([Value_1, Value_11, Value_12]);  Value_1[1] = Value_moy[1]
     
     # On plot les données dans un graphes
-    Value_1[0] -= 7.5
+    Value_1[0] -= 11
     Title = ['Bloc 3 : Détecteur de crète sans détections','V_C','V_L']
     cs.graph(Title, Value_1)
-plot_bloc3()
+
 def plot_bloc4():
     '''
     On plot le bloc 4
     '''
     # On calcule V_F et V_C
     (Title, Value_1) = cs.get_data(r"C:\Users\gasto\Downloads\Données final\VcR_VfB\VcR_VfB_01.csv")
-    (Title, Value_11) = cs.get_data(r"C:\Users\gasto\Downloads\Données final\VcR_VfB\VcR_VfB_02.csv")
-    (Title, Value_12) = cs.get_data(r"C:\Users\gasto\Downloads\Données final\VcR_VfB\VcR_VfB_03.csv")
     
     # On calcule V_sous    
     (Title, Value_2) = cs.get_data(r"C:\Users\gasto\Downloads\Données final\VcR_VsousB\VcR_VsousB_01.csv")
-    
+    (Title, Value_21) = cs.get_data(r"C:\Users\gasto\Downloads\Données final\VcR_VsousB\VcR_VsousB_01.csv")
+    (Title, Value_22) = cs.get_data(r"C:\Users\gasto\Downloads\Données final\VcR_VsousB\VcR_VsousB_01.csv")
+    # On fait la moyenne de V_sous
+    Value_2 = cs.data_moy([Value_2, Value_21, Value_22])
+
     # On plot les données dans un graphe
-    Value_1[0] -= 2.5
+    Value_1[0] -= 4.25;  Value_2[0] +=  2
     Title = ['Bloc 4 : Soustracteur','V_F','V_C', 'V_sous']
     fig, ax = plt.subplots()
-    fig.suptitle(Title[0])
     color = ['r', 'b', 'g', 'y', 'm', 'c', 'k']
     ax.plot(Value_1[0], Value_1[1], color[0], label = Title[1])
     ax.plot(Value_1[0] + 1, Value_1[2], color[1], label = Title[2])
     ax.plot(Value_2[0], Value_2[1], color[2], label = Title[3])
     ax.legend()
-    ax.set_ylim(-1,3)
-    ax.set_xlim(0, 25)
+    ax.set_ylim(-0.5,2.2)
+    ax.set_xlim(0, 20)
     ax.set_xlabel('Temps [µs]')
     ax.set_ylabel('Tensions [V]')
     plt.show()
@@ -122,7 +123,7 @@ def plot_bloc5():
     
     Title = ['Bloc 5 : Comparateur','V_F','V_out', 'V_ref']
     fig, ax = plt.subplots()
-    fig.suptitle(Title[0])
+
     color = ['r', 'b', 'g', 'y', 'm', 'c', 'k']
     ax.plot(Value_1[0], Value_1[1], color[0], label = Title[1])
     ax.plot(Value_1[0], Value_1[2], color[1], label = Title[2])
